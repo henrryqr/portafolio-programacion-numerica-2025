67c231c05d698136c586c2b72f85a91a54f7ed97
henrryqr <hequisper@est.unap.edu>
2025-09-17 15:52:55 -0500

    actualizar1

--- script.js
+++ script.js
@@ -1,25 +1,2 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-  const secciones = document.querySelectorAll("section, .card");
-
-  secciones.forEach((el, i) => {
-    setTimeout(() => {
-      el.style.opacity = "1";
-      el.style.transform = "translateY(0)";
-    }, i * 200);
-  });
-});
-
-// --- Efecto al pasar sobre las tarjetas ---
-const tarjetas = document.querySelectorAll(".card");
-
-tarjetas.forEach(card => {
-  card.addEventListener("mouseenter", () => {
-    card.style.transform = "scale(1.05)";
-    card.style.transition = "transform 0.3s ease";
-  });
-
-  card.addEventListener("mouseleave", () => {
-    card.style.transform = "scale(1)";
-  });
-});
+// Solo para prueba
+console.log("Portafolio cargado correctamente 🚀");
